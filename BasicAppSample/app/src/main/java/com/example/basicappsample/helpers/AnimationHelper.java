730f6bfa87da655b49aa35f18e305a454a88e326
LeoAndo <leo.777.info@gmail.com>
2022-01-14 01:53:56 +0900

    delete never used Method..

--- BasicAppSample/app/src/main/java/com/example/basicappsample/helpers/AnimationHelper.java
+++ BasicAppSample/app/src/main/java/com/example/basicappsample/helpers/AnimationHelper.java
@@ -17,37 +17,6 @@ public final class AnimationHelper {
     }
 
     /**
-     * 点滅アニメーションを行う.
-     *
-     * @param view アニメーションを適用させるView
-     */
-    public static void startAlphaAnimation(View view) {
-        final float fromAlpha = 0.0f, toAlpha = 1.0f;
-        final long durationMillis = 800, startOffset = 1000;
-        Animation animation = new AlphaAnimation(fromAlpha, toAlpha);
-        animation.setDuration(durationMillis);
-        animation.setStartOffset(startOffset);
-        animation.setRepeatMode(Animation.REVERSE);
-        animation.setRepeatCount(Animation.INFINITE);
-        view.startAnimation(animation);
-    }
-
-    /**
-     * Fade In / Fade outアニメーションを行う
-     *
-     * @param isVisible true: 表示 false: 非表示
-     * @param vg ConstraintLayoutやLinearLayoutなどのレイアウトクラスのインスタンス
-     * @param targetView アニメーションを適用させるView
-     */
-    public static void startFadeAnimation(boolean isVisible, ViewGroup vg, View targetView) {
-        MaterialFade fade = new MaterialFade();
-        fade.setDuration(500L);
-        TransitionManager.beginDelayedTransition(vg, fade);
-        int visibility = (isVisible) ? View.VISIBLE : View.INVISIBLE;
-        targetView.setVisibility(visibility);
-    }
-
-    /**
      * Springアニメーション(バネ)の生成
      *
      * @param view          アニメーションされるView
